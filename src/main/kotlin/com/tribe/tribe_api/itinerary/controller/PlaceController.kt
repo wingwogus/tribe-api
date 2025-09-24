dad12562f7f7024d091220c753f48b3230061ab4
monikhyun <mih2001103@naver.com>
2025-09-24 11:21:45 +0900

    Feat / 장소 검색 Api 구현 완료

---
+++ src/main/kotlin/com/tribe/tribe_api/itinerary/controller/PlaceController.kt
@@ -0,0 +1,30 @@
+package com.tribe.tribe_api.itinerary.controller
+
+import com.tribe.tribe_api.common.util.ApiResponse
+import com.tribe.tribe_api.itinerary.dto.PlaceDto
+import com.tribe.tribe_api.itinerary.service.GoogleMapService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/places")
+class PlaceController(
+    private val googleMapService: GoogleMapService
+) {
+    @GetMapping("/search")
+    fun searchPlaces(
+        @RequestParam query: String?,
+        @RequestParam region: String?,
+        @RequestParam(defaultValue = "ko") language: String
+
+    ): ResponseEntity<ApiResponse<List<PlaceDto.Simple>>> {
+
+
+        val response = googleMapService.searchPlaces(query, language, region)
+
+        return ResponseEntity.ok(ApiResponse.success(response))
+    }
+}
