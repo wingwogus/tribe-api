01652ce84e3f6ac3d91dad83d282fb1244a358f4
문익현 <97443699+monikhyun@users.noreply.github.com>
2025-09-24 13:15:03 +0900

    fix / Update TribeApiApplication.kt

--- src/main/kotlin/com/tribe/tribe_api/TribeApiApplication.kt
+++ src/main/kotlin/com/tribe/tribe_api/TribeApiApplication.kt
@@ -2,8 +2,10 @@ package com.tribe.tribe_api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class TribeApiApplication
 
 fun main(args: Array<String>) {
