4cc286ab7e743ed9819e1a812a80011021ae8c91
monikhyun <mih2001103@naver.com>
2025-09-24 11:21:05 +0900

    Feat / Application 어노테이션 추가

--- src/main/kotlin/com/tribe/tribe_api/TribeApiApplication.kt
+++ src/main/kotlin/com/tribe/tribe_api/TribeApiApplication.kt
@@ -2,10 +2,8 @@ package com.tribe.tribe_api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
-@EnableJpaAuditing
 class TribeApiApplication
 
 fun main(args: Array<String>) {
