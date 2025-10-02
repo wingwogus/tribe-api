5ebb80139054da90cf027d900eaa6ebbab7b6692
Hyeni <jjang010630@naver.com>
2025-10-03 01:48:12 +0900

    feat(Expense): paymentDate, receiptImageUrl 필드 추가

--- src/main/kotlin/com/tribe/tribe_api/expense/entity/Expense.kt
+++ src/main/kotlin/com/tribe/tribe_api/expense/entity/Expense.kt
@@ -7,6 +7,7 @@ import com.tribe.tribe_api.trip.entity.Trip
 import com.tribe.tribe_api.trip.entity.TripMember
 import jakarta.persistence.*
 import java.math.BigDecimal
+import java.time.LocalDate
 
 @Entity
 class Expense(
@@ -20,7 +21,7 @@ class Expense(
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "payer_id", nullable = false)
-    val payer: TripMember,
+    var payer: TripMember,
 
     var title: String,
 
@@ -29,6 +30,12 @@ class Expense(
     @Enumerated(EnumType.STRING)
     @Column(name = "entry_method", nullable = false)
     var entryMethod: InputMethod,
+
+    @Column(name = "payment_date", nullable = true)
+    var paymentDate: LocalDate,
+
+    @Column(name = "receipt_image_url", nullable = true)
+    var receiptImageUrl: String? = null,
 
 ) : BaseTimeEntity() {
     @Id
