337d9eb79f54a69b3e402d5b2cfc31ce5bf2f7f1
gerrmang <tjdtlghks11@naver.com>
2025-10-02 16:36:20 +0900

    필요해서 만든 place리파지토리

---
+++ src/main/kotlin/com/tribe/tribe_api/itinerary/repository/PlaceRepository.kt
@@ -0,0 +1,7 @@
+package com.tribe.tribe_api.itinerary.repository
+
+import com.tribe.tribe_api.itinerary.entity.Place
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PlaceRepository : JpaRepository<Place, Long> {
+}
