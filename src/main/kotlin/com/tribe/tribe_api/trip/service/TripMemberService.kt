119677948a9ef720557b232d5c177788577ba516
Hyeni <jjang010630@naver.com>
2025-10-18 02:38:57 +0900

    feat(TripMemberService):검증 로직 추가

--- src/main/kotlin/com/tribe/tribe_api/trip/service/TripMemberService.kt
+++ src/main/kotlin/com/tribe/tribe_api/trip/service/TripMemberService.kt
@@ -2,24 +2,29 @@ package com.tribe.tribe_api.trip.service
 
 import com.tribe.tribe_api.common.exception.BusinessException
 import com.tribe.tribe_api.common.exception.ErrorCode
+import com.tribe.tribe_api.common.util.service.TripSecurityService
 import com.tribe.tribe_api.trip.dto.TripMemberDto
 import com.tribe.tribe_api.trip.entity.TripMember
 import com.tribe.tribe_api.trip.entity.TripRole
 import com.tribe.tribe_api.trip.repository.TripMemberRepository
 import com.tribe.tribe_api.trip.repository.TripRepository
-import jakarta.persistence.EntityNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class TripMemberService(
     private val tripRepository: TripRepository,
-    private val tripMemberRepository: TripMemberRepository
+    private val tripMemberRepository: TripMemberRepository,
+    private val tripSecurityService: TripSecurityService
 ) {
 
     //특정 여행에 임시 참여자(게스트)를 추가
     @Transactional
-    fun addGuest(tripId: Long, request: TripMemberDto.AddGuestRequest): TripMemberDto.Info {
+    fun addGuest(request: TripMemberDto.AddGuestRequest): TripMemberDto.Info {
+        val tripId = request.tripId
+
+        tripSecurityService.isTripMember(tripId)
+
         val trip = tripRepository.findById(tripId)
             .orElseThrow { BusinessException(ErrorCode.TRIP_NOT_FOUND) }
 
@@ -29,6 +34,7 @@ class TripMemberService(
             guestNickname = request.name,
             role = TripRole.GUEST
         )
+        trip.members.add(newGuest)
 
         val savedGuest = tripMemberRepository.save(newGuest)
         return TripMemberDto.Info.from(savedGuest)
