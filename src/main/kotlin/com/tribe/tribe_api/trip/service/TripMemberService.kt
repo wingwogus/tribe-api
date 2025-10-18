44f1b727e810c6d7493d3344c95a61e378d4c257
Hyeni <jjang010630@naver.com>
2025-10-18 22:01:48 +0900

    fix(TripMemberService): @PreAuthorize 사용

--- src/main/kotlin/com/tribe/tribe_api/trip/service/TripMemberService.kt
+++ src/main/kotlin/com/tribe/tribe_api/trip/service/TripMemberService.kt
@@ -2,28 +2,26 @@ package com.tribe.tribe_api.trip.service
 
 import com.tribe.tribe_api.common.exception.BusinessException
 import com.tribe.tribe_api.common.exception.ErrorCode
-import com.tribe.tribe_api.common.util.service.TripSecurityService
 import com.tribe.tribe_api.trip.dto.TripMemberDto
 import com.tribe.tribe_api.trip.entity.TripMember
 import com.tribe.tribe_api.trip.entity.TripRole
 import com.tribe.tribe_api.trip.repository.TripMemberRepository
 import com.tribe.tribe_api.trip.repository.TripRepository
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class TripMemberService(
     private val tripRepository: TripRepository,
-    private val tripMemberRepository: TripMemberRepository,
-    private val tripSecurityService: TripSecurityService
+    private val tripMemberRepository: TripMemberRepository
 ) {
 
     //특정 여행에 임시 참여자(게스트)를 추가
     @Transactional
+    @PreAuthorize("@tripSecurityService.isTripMember(tripId)")
     fun addGuest(request: TripMemberDto.AddGuestRequest): TripMemberDto.Info {
         val tripId = request.tripId
-
-        tripSecurityService.isTripMember(tripId)
 
         val trip = tripRepository.findById(tripId)
             .orElseThrow { BusinessException(ErrorCode.TRIP_NOT_FOUND) }
