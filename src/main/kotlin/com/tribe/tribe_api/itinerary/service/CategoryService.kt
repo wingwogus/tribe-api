c9e460093d84c3fa2742b781148d3ee80e60ffde
monikhyun <mih2001103@naver.com>
2025-10-29 14:42:42 +0900

    Feat / orderUpdateCategory추가

--- src/main/kotlin/com/tribe/tribe_api/itinerary/service/CategoryService.kt
+++ src/main/kotlin/com/tribe/tribe_api/itinerary/service/CategoryService.kt
@@ -84,4 +84,53 @@ class CategoryService (
         }
         categoryRepository.deleteById(categoryId)
     }
+
+    fun orderUpdateCategory(tripId: Long, day: Int? ,request : CategoryDto.OrderUpdate) : List<CategoryDto.CategoryResponse> {
+        val memberId = SecurityUtil.getCurrentMemberId()
+
+
+        val requestItems = request.items
+
+        val isTripMember = tripMemberRepository.existsByTripIdAndMemberId(tripId, memberId)
+        if (!isTripMember) {
+            throw BusinessException(ErrorCode.NOT_A_TRIP_MEMBER)
+        }
+
+        val categoryIds = request.items.map { it.categoryId }
+
+        // 요청받은 ID와 새 order 값을 Map으로 변환
+        val newOrderMap = request.items.associateBy({ it.categoryId }, { it.order })
+
+        if (newOrderMap.size != requestItems.size) {
+            throw BusinessException(ErrorCode.DUPLICATE_CATEGORY_ID_REQUEST)
+        }
+
+        val uniqueOrders = newOrderMap.values.toSet()
+        if (uniqueOrders.size != newOrderMap.size) {
+            throw BusinessException(ErrorCode.DUPLICATE_ORDER_REQUEST)
+        }
+
+        // DB에서 해당 여행(tripId)에 속한 카테고리 중, 요청받은 ID 목록에 해당하는 것들만 조회
+        val categoriesToUpdate: List<Category> = if (day != null) {
+            // day가 있는 경우: tripId, day, id 목록 모두 일치하는 것 조회
+            categoryRepository.findAllByTripIdAndDayAndIdIn(tripId, day, categoryIds)
+        } else {
+            // day가 null인 경우: tripId, day가 null, id 목록 모두 일치하는 것 조회
+            categoryRepository.findAllByTripIdAndDayIsNullAndIdIn(tripId, categoryIds)
+        }
+
+        if (categoriesToUpdate.size != newOrderMap.size) {
+            throw BusinessException(ErrorCode.CATEGORY_NOT_FOUND)
+        }
+
+        // 조회된 카테고리들의 order 값을 Map에 있는 새 order 값으로 업데이트
+        categoriesToUpdate.forEach { category ->
+            val newOrder = newOrderMap[category.id]!!
+            category.updateOrder(newOrder)
+        }
+        //순서가 변경된 카테고리 목록을 다시 조회하여 반환
+        return categoriesToUpdate
+            .sortedBy { it.order }
+            .map { CategoryDto.CategoryResponse.from(it) }
+    }
 }
