0ed7bc9a023fcbfc299013db18f4d12a5c4c90d8
Hyeni <jjang010630@naver.com>
2025-11-06 21:36:26 +0900

    fix(ExpenseRepository): paymentDate를 사용하는 쿼리문 삭제, findAllWithDetailsByTripId로 통합

--- src/main/kotlin/com/tribe/tribe_api/expense/repository/ExpenseRepository.kt
+++ src/main/kotlin/com/tribe/tribe_api/expense/repository/ExpenseRepository.kt
@@ -3,7 +3,6 @@ package com.tribe.tribe_api.expense.repository
 import com.tribe.tribe_api.expense.entity.Expense
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import java.time.LocalDate
 
 interface ExpenseRepository : JpaRepository<Expense, Long> {
 
