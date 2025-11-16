0b6f9162ded56a1a8ee9bf05879e3e495ec3c512
Hyeni <jjang010630@naver.com>
2025-11-16 22:09:05 +0900

    feat(TripMemberController): 임시참여자 삭제, 강퇴, 여행 탈퇴 기능 추가

--- src/main/kotlin/com/tribe/tribe_api/trip/controller/TripMemberController.kt
+++ src/main/kotlin/com/tribe/tribe_api/trip/controller/TripMemberController.kt
@@ -6,13 +6,14 @@ import com.tribe.tribe_api.trip.service.TripMemberService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/expenses")
+@RequestMapping("/api/v1/trips")
 class TripMemberController (
     private val tripMemberService: TripMemberService
 ){
@@ -26,5 +27,35 @@ class TripMemberController (
         return ResponseEntity
             .status(HttpStatus.CREATED)
             .body(ApiResponse.success("게스트 추가 성공", response))
+    }
+
+    // 게스트 삭제
+    @DeleteMapping("/members/guest")
+    fun deleteGuest(
+        @Valid @RequestBody request: TripMemberDto.DeleteGuestRequest
+    ): ResponseEntity<ApiResponse<Any>> {
+        tripMemberService.deleteGuest(request.tripId, request.guestTripMemberId)
+        return ResponseEntity
+            .ok(ApiResponse.success("게스트 삭제 성공"))
+    }
+
+    // OWNER가 특정 MEMBER 강퇴
+    @DeleteMapping("/members/kick")
+    fun kickMember(
+        @Valid @RequestBody request: TripMemberDto.KickMemberRequest
+    ): ResponseEntity<ApiResponse<Any>> {
+        tripMemberService.kickMember(request.tripId, request.targetMemberId)
+        return ResponseEntity
+            .ok(ApiResponse.success("멤버 강퇴 성공"))
+    }
+
+    // 여행 탈퇴
+    @DeleteMapping("/members/leave")
+    fun leaveTrip(
+        @Valid @RequestBody request: TripMemberDto.LeaveTripRequest
+    ): ResponseEntity<ApiResponse<Any>> {
+        tripMemberService.leaveTrip(request.tripId, request.memberId)
+        return ResponseEntity
+            .ok(ApiResponse.success("여행 탈퇴 성공"))
     }
 }
