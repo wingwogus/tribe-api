012c510a1d0d6c4b967a7b13e5b999e38e9deede
Hyeni <jjang010630@naver.com>
2025-11-16 21:59:25 +0900

    feat(ExpenseAssignmentRepository): 탈퇴멤버가 갚아야하는 모든 배분 정보를 삭제

--- src/main/kotlin/com/tribe/tribe_api/expense/repository/ExpenseAssignmentRepository.kt
+++ src/main/kotlin/com/tribe/tribe_api/expense/repository/ExpenseAssignmentRepository.kt
@@ -7,12 +7,14 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.transaction.annotation.Transactional
 
 interface ExpenseAssignmentRepository : JpaRepository<ExpenseAssignment, Long> {
-    @Transactional
-    @Modifying
-    @Query("delete from ExpenseAssignment ea where ea.expenseItem.expense.id = :expenseId")
-    fun deleteByExpenseId(expenseId: Long)
 
     @Transactional
     @Modifying
     fun deleteByExpenseItemId(itemId: Long)
+
+    // 나가는 멤버가 갚아야 할 사람인 모든 배분 정보를 삭제
+    @Transactional
+    @Modifying
+    @Query("delete from ExpenseAssignment ea where ea.debtor.id = :memberId")
+    fun deleteByDebtorId(memberId: Long)
 }
