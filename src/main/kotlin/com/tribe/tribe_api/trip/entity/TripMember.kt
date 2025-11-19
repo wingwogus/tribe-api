a9eaf87707889e2bc2970427afac549267a2dc4c
monikhyun <mih2001103@naver.com>
2025-11-19 11:27:43 +0900

    Feat / 멤버 권한변경 기능 추가

--- src/main/kotlin/com/tribe/tribe_api/trip/entity/TripMember.kt
+++ src/main/kotlin/com/tribe/tribe_api/trip/entity/TripMember.kt
@@ -20,7 +20,7 @@ class TripMember(
     var guestNickname: String? = null,
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, length = 10)
     var role: TripRole,
 ) {
     @Id
