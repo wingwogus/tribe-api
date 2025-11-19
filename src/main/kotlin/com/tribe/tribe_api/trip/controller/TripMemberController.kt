bbb095433ed1615221224d29b45f76d20b8de93b
monikhyun <mih2001103@naver.com>
2025-11-19 11:30:21 +0900

    Feat / 멤버권한 수정 엔드포인트 추가

--- src/main/kotlin/com/tribe/tribe_api/trip/controller/TripMemberController.kt
+++ src/main/kotlin/com/tribe/tribe_api/trip/controller/TripMemberController.kt
@@ -6,18 +6,19 @@ import com.tribe.tribe_api.trip.service.TripMemberService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/expenses")
-class TripMemberController (
+@RequestMapping("/api/v1/trip-members")
+class TripMemberController(
     private val tripMemberService: TripMemberService
-){
+) {
     // 게스트 추가
-    @PostMapping("/guest")
+    @PostMapping("/expenses/guest")
     fun addGuest(
         @Valid @RequestBody request: TripMemberDto.AddGuestRequest
     ): ResponseEntity<ApiResponse<TripMemberDto.Simple>> {
@@ -26,5 +27,14 @@ class TripMemberController (
         return ResponseEntity
             .status(HttpStatus.CREATED)
             .body(ApiResponse.success("게스트 추가 성공", response))
+    }
+
+    // 멤버 권한 수정
+    @PatchMapping("/role")
+    fun assignRole(
+        @Valid @RequestBody request: TripMemberDto.AssignRoleRequest
+    ) : ResponseEntity<ApiResponse<TripMemberDto.Simple>>{
+        val response = tripMemberService.assignRole(request.tripId, request)
+        return ResponseEntity.ok(ApiResponse.success("멤버 권한 수정 성공", response))
     }
 }
