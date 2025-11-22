613a4b38d13fe5397346daf072e864cd26b5f7a1
Hyeni <jjang010630@naver.com>
2025-11-22 14:52:52 +0900

    fix(ExpenseAssignmentRepository): 안쓰는 메소드 삭제, 특정 멤버가 포함된 모든 분배 내역 조회, 특정 지출 항목에서 특정 멤버의 분배 내역만 삭제 메소드 추가

--- src/main/kotlin/com/tribe/tribe_api/expense/repository/ExpenseAssignmentRepository.kt
+++ src/main/kotlin/com/tribe/tribe_api/expense/repository/ExpenseAssignmentRepository.kt
@@ -3,7 +3,6 @@ package com.tribe.tribe_api.expense.repository
 import com.tribe.tribe_api.expense.entity.ExpenseAssignment
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
 import org.springframework.transaction.annotation.Transactional
 
 interface ExpenseAssignmentRepository : JpaRepository<ExpenseAssignment, Long> {
@@ -12,7 +11,9 @@ interface ExpenseAssignmentRepository : JpaRepository<ExpenseAssignment, Long> {
     @Modifying
     fun deleteByExpenseItemId(itemId: Long)
 
-    @Modifying
-    @Query("UPDATE ExpenseAssignment a SET a.tripMember = null WHERE a.tripMember.id = :exitingMemberId")
-    fun setTripMemberToNullForExitingMember(exitingMemberId: Long)
+    // 특정 멤버가 포함된 모든 분배 내역 조회
+    fun findAllByTripMemberId(tripMemberId: Long): List<ExpenseAssignment>
+
+    // 특정 지출 항목에서 특정 멤버의 분배 내역만 삭제
+    fun deleteByExpenseItemIdAndTripMemberId(expenseItemId: Long, tripMemberId: Long)
 }
